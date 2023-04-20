f0fd1689be6e65703f844e9ee834c8b76956d119
seema5813 <s5813seema@gmail.com>
2023-04-20 21:07:37 +0530

    news api project added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect,useState} from 'react'
 
-function App() {
+import axios from 'axios'
+
+//create a function
+function App(){
+  let[data,setdata]=useState([])
+  useEffect(()=>{
+    // let link = 'https://newsapi.org/v2/everything?q=tesla&from=2023-03-19&sortBy=publishedAt&apiKey=1daf07f41981406f803e0b1440eb40cd'
+    // axios(link).then((result)=>{
+      
+    let res = axios('https://newsapi.org/v2/everything?q=tesla&from=2023-03-19&sortBy=publishedAt&apiKey=1daf07f41981406f803e0b1440eb40cd')
+      
+      res.then((result) =>{
+      // console.log(result.data.articles);
+      setdata(result.data.articles)
+  })
+    })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <h2>App component</h2>
+    {data?<div>
+      {
+        data.map((item)=>{
+          return(
+            <div>
+              <h2>{item.title}</h2>
+              <img src={item.urlToImage} height="400px" width="60%" alt="...."/>
+              <h2>{item.author}</h2>
+
+            </div>
+          )
+          })
+      }
+    </div>:<p>No data</p>}
+    </>
+  )
 }
 
-export default App;
+export default App
